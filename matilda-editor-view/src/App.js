bb60384a6c17c4f4290d038d8c091524e11d924d
Darren Littlejohn <darrenlittlejohn@Darrens-MacBook-Pro.local>
2017-11-07 13:27:01 -0800

    Search Featur

--- matilda-editor-view/src/App.js
+++ matilda-editor-view/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import SearchTitle from './pages/SearchTitle'
+import Display from './pages/Display'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+constructor(props) {
+  	super(props);
+  	this.state = {
+		results: []
+	}
 }
+	updateResults(newResults){
+   	this.setState({results: newResults})
+ }
+	render(){
+  		return(
+			<div>
+				<SearchTitle onSearch = {this.updateResults.bind(this)}/>
+				<Display books = {this.state.results} />
+			</div>
+  		)
+	}
+}
+
 
 export default App;
---
+++ matilda-editor-view/src/scratch.js
@@ -0,0 +1,9 @@
+
+	constructor(props) {
+	  	super(props);
+	  	this.state = {
+            searchText: '',
+		}
+
+
+		import {FormGroup, FormControl, formInstance, mountNode} from 'react-bootstrap'
