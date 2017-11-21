6816df301f308d70043d7c4c1eb51f8e70b9ab0c
jtran1203 <tranjohn1211@gmail.com>
2017-11-21 10:59:27 -0800

    editing on new branch

--- matilda-editor-view/src/pages/Display.js
+++ matilda-editor-view/src/pages/Display.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
+import BookDisplay from './BookDisplay';
+
 
 class Display extends Component {
-	render() {
-    var data = this.props.books
-    var resultDisplay = data.map( (book,key)=>{
-      let lexile = book.lexile_score
-      if (lexile == null){
-        lexile= 'no information'
-      }
-      let variety= book.text_variety
-      if (variety == null){
-        variety= 'no information'
-      }
-        return (
-          <div className="Display">
-            <p>Title: {book.title}</p>
-            <p>Author: {book.author_full_name}</p>
-            <p>Lexile: {lexile}</p>
-            <p>Text Variety: {variety}</p>
-            <img src={book.cover_url} />
-          </div>
-        )
-      }
-    )
-    return (
-      <div>
-        {resultDisplay}
-      </div>
-    )
+
+	render (){
+		var data = this.props.books
+		var resultDisplay = data.map( (book,key)=>{
+			return (
+			<BookDisplay book = {book} />
+		)}
+		)
+		return (
+			<div>
+			{resultDisplay}
+
+			</div>
+		)
 	}
 }
 
